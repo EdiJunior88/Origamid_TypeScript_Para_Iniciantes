cd4f99c8ffe07298747e762f73bfe05c7de5729d
EdiJunior88 <er.moura88@yahoo.com.br>
2023-08-10 10:01:22 -0300

    0101 TypeScript para Iniciantes 0102 Types 0103 Type Annotation

--- curso/src/main.js
+++ curso/src/main.js
@@ -1,34 +1,22 @@
 //@ts-check
 
-const frase = "Front End";
-const total = 100.05;
-const empresas = ["Apple", "Microsoft", "Google", "Facebook"];
+const produtos = [
+  {
+    nome: "O Senhor dos Anéis",
+    tipo: "livro",
+  },
+  {
+    nome: "A Guerra dos Tronos",
+    tipo: "livro",
+  },
+  {
+    nome: "Dark Souls",
+    tipo: "jogo",
+  },
+];
 
-console.log(frase.length);
-console.log(frase.charAt(0));
-
-console.log(frase.toLowerCase());
-
-// console.log(total.toLowerCase());
-
-const t = total.toFixed();
-
-console.log(t + 10);
-
-const array = empresas.map((empresa) => empresa.toUpperCase());
-
-console.log(array);
-
-const body = document.body;
-
-body.style.background = "#eee";
-
-const button = document.querySelector("button");
-
-if (button) {
-  button.click();
+function filtrarLivros(dados) {
+  return dados.filter((item) => item.tipo === "livro");
 }
 
-const operacao = 100 + 200;
-
-console.log(operacao);
+console.log(filtrarLivros(produtos));
