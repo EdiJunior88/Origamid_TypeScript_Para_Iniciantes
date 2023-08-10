61cd335e3b7f208e5370f74f318ad2924c308c3d
EdiJunior88 <er.moura88@yahoo.com.br>
2023-08-09 22:05:55 -0300

    0101 TypeScript para Iniciantes 0102 Types

--- curso/src/main.js
+++ curso/src/main.js
@@ -1,1 +1,34 @@
-console.log("Hello World");
+//@ts-check
+
+const frase = "Front End";
+const total = 100.05;
+const empresas = ["Apple", "Microsoft", "Google", "Facebook"];
+
+console.log(frase.length);
+console.log(frase.charAt(0));
+
+console.log(frase.toLowerCase());
+
+// console.log(total.toLowerCase());
+
+const t = total.toFixed();
+
+console.log(t + 10);
+
+const array = empresas.map((empresa) => empresa.toUpperCase());
+
+console.log(array);
+
+const body = document.body;
+
+body.style.background = "#eee";
+
+const button = document.querySelector("button");
+
+if (button) {
+  button.click();
+}
+
+const operacao = 100 + 200;
+
+console.log(operacao);
