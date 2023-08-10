031936541bcc8ca04f89300b0e40c524f21db3fe
EdiJunior88 <er.moura88@yahoo.com.br>
2023-08-10 10:22:28 -0300

    0101 TypeScript para Iniciantes 0102 Types 0103 Type Annotation 0104 TypeScript Ferramentas 02 TypeScript Básico 0201 Annotation e Inference 1

--- curso/src/main.ts
+++ curso/src/main.ts
@@ -1,7 +1,29 @@
-const total = 100;
+let produto = "Livro";
+let preco = 200;
+
+const carro = {
+  marca: "Audi",
+  portas: 5,
+};
+
+const barato = 200 < 400 ? true : "produto caro";
 
 function somar(a: number, b: number) {
   return a + b;
 }
 
-console.log(somar(5, 10));
+somar(4, 10);
+// somar (4, "10")
+
+const Nintendo = {
+  nome: "Nintendo",
+  preco: "2000",
+};
+
+function transformarPreco(produto: { nome: string; preco: string }) {
+  produto.preco = "R$ " + produto.preco;
+  return produto
+}
+
+const produtoNovo = transformarPreco(Nintendo)
+console.log(produtoNovo);
