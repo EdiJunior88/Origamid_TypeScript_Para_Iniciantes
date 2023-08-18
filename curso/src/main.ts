8511ea33feff8743e84bbe89e723abaca6401cce
EdiJunior88 <er.moura88@yahoo.com.br>
2023-08-18 17:03:35 -0300

    0101 TypeScript para Iniciantes 0102 Types 0103 Type Annotation 0104 TypeScript Ferramentas
    02 TypeScript Básico
    0201 Annotation e Inference 1
    0201 Annotation e Inference 2
    0202 String, Number e Boolean
    0203 Union Types 1
    0203 Union Types 2
    0204 Types e Interfaces 1
    0204 Types e Interfaces 2
    0204 Types e Interfaces 3
    0205 Arrays 1
    0205 Arrays 2

--- curso/src/main.ts
+++ curso/src/main.ts
@@ -1,18 +1,42 @@
-const numeros = [10, 20, 30, 40, 50, 3];
-const valores = [10, "Taxas", 30, "Produto", 50, 3];
+// Defina a interface da API: https://api.origamid.dev/json/cursos.json e mostre os dados na tela.
 
-function maiorQue10(data: Array<number>) {
-  return data.filter((n) => n > 10);
+// Existem apenas dois níveis de cursos, Iniciante (iniciante) e Avançado (avancado). Se for para iniciante pinte o título de azul, para avançado pinte de vermelho.
+
+interface Curso {
+  nome: string;
+  horas: number;
+  aulas: string;
+  gratuito: boolean;
+  tags: string[];
+  idAulas: number[];
+  nivel: "iniciante" | "avancado";
 }
 
-function filtrarValores(data: Array<number | string>) {
-  return data.filter((n) => typeof n === "number");
+async function fetchCursos() {
+  const response = await fetch("https://api.origamid.dev/json/cursos.json");
+  const data = await response.json();
+  mostrarCursos(data);
 }
 
-console.log(filtrarValores(valores));
-console.log(maiorQue10(numeros));
+fetchCursos();
 
-const dados = [
-  ["senhor dos aneis", 80],
-  ["a guerra dos tronos", 120],
-];
+function mostrarCursos(cursos: Curso[]) {
+  cursos.forEach((curso) => {
+    let color;
+    if (curso.nivel === "iniciante") {
+      color = "blue";
+    } else if (curso.nivel === "avancado") {
+      color = "red";
+    }
+    document.body.innerHTML += `
+      <div>
+        <h2 style="color: ${color};">${curso.nome}</h2>
+        <p>Horas: ${curso.horas}</p>
+        <p>Aulas: ${curso.aulas}</p>
+        <p>Tipo: ${curso.gratuito ? "Gratuito" : "Pago"}</p>
+        <p>Tags: ${curso.tags.join(", ")}</p>
+        <p>Aulas: ${curso.idAulas.join(" | ")}</p>
+      </div>
+    `;
+  });
+}
