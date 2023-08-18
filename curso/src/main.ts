abf3ceb6eb4cbb5bd22d349b3bdf89e0c4830395
EdiJunior88 <er.moura88@yahoo.com.br>
2023-08-18 17:01:00 -0300

    0101 TypeScript para Iniciantes 0102 Types 0103 Type Annotation 0104 TypeScript Ferramentas
    02 TypeScript Básico
    0201 Annotation e Inference 1
    0201 Annotation e Inference 2
    0202 String, Number e Boolean
    0203 Union Types 1
    0203 Union Types 2
    0204 Types e Interfaces 1
    0204 Types e Interfaces 2
    0204 Types e Interfaces 3
    0205 Arrays 1

--- curso/src/main.ts
+++ curso/src/main.ts
@@ -1,40 +1,18 @@
-// Defina a interface da API: https://api.origamid.dev/json/notebook.json e mostre os dados na tela.
+const numeros = [10, 20, 30, 40, 50, 3];
+const valores = [10, "Taxas", 30, "Produto", 50, 3];
 
-interface Empresa {
-  nome: string;
-  fundacao: number;
-  pais: string;
+function maiorQue10(data: Array<number>) {
+  return data.filter((n) => n > 10);
 }
 
-interface Product {
-  nome: string;
-  preco: number;
-  descricao: string;
-  garantia: string;
-  seguroAcidentes: boolean;
-  empresaFabricante: Empresa;
-  empresaMontadora: Empresa;
+function filtrarValores(data: Array<number | string>) {
+  return data.filter((n) => typeof n === "number");
 }
 
-async function fetchProduct() {
-  const response = await fetch('https://api.origamid.dev/json/notebook.json');
-  const data = await response.json();
-  showProduct(data);
-}
+console.log(filtrarValores(valores));
+console.log(maiorQue10(numeros));
 
-fetchProduct();
-
-function showProduct(data: Product) {
-  document.body.innerHTML = `
-    <div>
-      <h2>${data.nome}</h2>
-      <p>R$ ${data.preco}</p>
-      <div>
-        <h3>Fabricante: ${data.empresaFabricante.nome}</h3>
-      </div>
-      <div>
-        <h3>Montadora: ${data.empresaMontadora.nome}</h3>
-      </div>
-    </div>
-  `;
-}
+const dados = [
+  ["senhor dos aneis", 80],
+  ["a guerra dos tronos", 120],
+];
