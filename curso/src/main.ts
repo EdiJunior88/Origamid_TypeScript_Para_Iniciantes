8c970fa05496912d1f77c0be6eba8ac68321ba1b
EdiJunior88 <er.moura88@yahoo.com.br>
2023-08-21 18:04:50 -0300

    0101 TypeScript para Iniciantes 0102 Types 0103 Type Annotation 0104 TypeScript Ferramentas
    02 TypeScript Básico
    0201 Annotation e Inference 1
    0201 Annotation e Inference 2
    0202 String, Number e Boolean
    0203 Union Types 1
    0203 Union Types 2
    0204 Types e Interfaces 1
    0204 Types e Interfaces 2
    0204 Types e Interfaces 3
    0205 Arrays 1
    0205 Arrays 2
    0206 Any
    0207 Null e Undefined
    
    03 Objetos e Interfaces
    0301 Instanceof 1
    0301 Instanceof 2
    0301 Instanceof 3

--- curso/src/main.ts
+++ curso/src/main.ts
@@ -1,61 +1,9 @@
-class Produto {
-  nome: string;
+// 1 - Selecione o link utilizando o método getElementById.
 
-  constructor(nome: string) {
-    this.nome = nome;
-  }
+// 2 - Substitua o href do link (HTMLAnchorElement) de http:// para https://.const link = document.getElementById('origamid');
+
+const link = document.getElementById("origamid");
+
+if (link instanceof HTMLAnchorElement) {
+  link.href = link.href.replace("http://", "https://");
 }
-
-class Livro extends Produto {
-  autor: string;
-
-  constructor(nome: string, autor: string) {
-    super(nome);
-    this.autor = autor;
-  }
-}
-
-class Jogo extends Produto {
-  jogadores: number;
-
-  constructor(nome: string, jogadores: number) {
-    super(nome);
-    this.jogadores = jogadores;
-  }
-}
-
-function buscarProduto(busca: string) {
-  if (busca === "O Hobbit") {
-    return new Livro("O Hobbit", "J.R.R. Tolkien");
-  }
-
-  if (busca === "Dark Souls") {
-    return new Jogo("Dark Souls", 1);
-  }
-
-  return null;
-}
-
-const produto = buscarProduto("Dark Souls");
-
-if (produto instanceof Livro) {
-  console.log(produto.autor);
-}
-
-// if (produto instanceof Jogo) {
-//   console.log(produto.nome);
-// }
-
-if (produto instanceof Produto) {
-  console.log(produto.nome);
-}
-
-interface Carro {
-  nome: string;
-}
-
-const honda: Carro = {
-  nome: "Honda",
-};
-
-console.log(honda);
