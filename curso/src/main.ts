97d30a3fbfe4837ac182859176283f02f426996d
EdiJunior88 <er.moura88@yahoo.com.br>
2023-08-21 18:21:31 -0300

    0101 TypeScript para Iniciantes 0102 Types 0103 Type Annotation 0104 TypeScript Ferramentas
    02 TypeScript Básico
    0201 Annotation e Inference 1
    0201 Annotation e Inference 2
    0202 String, Number e Boolean
    0203 Union Types 1
    0203 Union Types 2
    0204 Types e Interfaces 1
    0204 Types e Interfaces 2
    0204 Types e Interfaces 3
    0205 Arrays 1
    0205 Arrays 2
    0206 Any
    0207 Null e Undefined
    
    03 Objetos e Interfaces
    0301 Instanceof 1
    0301 Instanceof 2
    0301 Instanceof 3
    0302 Interfaces do DOM 1
    0302 Interfaces do DOM 2

--- curso/src/main.ts
+++ curso/src/main.ts
@@ -1,5 +1,16 @@
-const video = document.querySelector("#videoprincipal");
+const links = document.querySelectorAll(".link");
 
-if (video instanceof HTMLVideoElement) {
-  console.log(video.volume);
-}
+links.forEach((link) => {
+  if (link instanceof HTMLAnchorElement) {
+    console.log(link.href);
+  }
+});
+
+const dados = [1, 2, 3];
+
+const arrayLinks = Array.from(links);
+const anchorLinks = arrayLinks.filter(
+  (link) => link instanceof HTMLAnchorElement
+);
+
+console.log(anchorLinks);
